75cffcda19aed0e1aab93448fa492c02e143df9d
LuckerChar <kokakolka85@gmail.com>
2022-08-06 16:15:49 +0300

    v.1 Work

--- src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ru.kata.spring.boot_security.demo.configs.WebSecurityConfig;
 import ru.kata.spring.boot_security.demo.models.User;
 import ru.kata.spring.boot_security.demo.repositories.RoleRepository;
 import ru.kata.spring.boot_security.demo.repositories.UserRepository;
@@ -18,21 +19,23 @@ public class UserServiceImpl implements UserService, UserDetailsService {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
 
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
-
+    private WebSecurityConfig webSecurityConfig;
+    @Autowired
+    public WebSecurityConfig setWebSecurityConfig() {
+        return webSecurityConfig;
+    }
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository) {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
-        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
 
     @Transactional
     @Override
     public void saveUser(User user) {
-        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setPassword(setWebSecurityConfig().passwordEncoder().encode(user.getPassword()));
         userRepository.save(user);
     }
 
@@ -40,7 +43,7 @@ public class UserServiceImpl implements UserService, UserDetailsService {
     @Override
     public void updateUser(long id, User user) {
         if (!user.getPassword().equals(userRepository.findById(id).get().getPassword())) {
-            user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+            user.setPassword(setWebSecurityConfig().passwordEncoder().encode(user.getPassword()));
         }
         userRepository.save(user);
     }
