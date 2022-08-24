c355b865795c3793697c8fb506493d8f84d59f55
LuckerChar <kokakolka85@gmail.com>
2022-08-24 11:26:26 +0300

    New Work JS v 1.0 Update12

--- src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
@@ -90,8 +90,8 @@ public class UserServiceImpl implements UserDetailsService, UserService {
     }
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username);
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(email);
         if (user == null) {
             throw new UsernameNotFoundException("User not found");
         }
